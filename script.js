34c0f0be7d43231ef6ff62bd02ccef30a88a4ef6
Am1d1v <dimadiman1adima@gmail.com>
2024-02-26 15:21:40 +0300

    Split Screen Effect

--- script.js
+++ script.js
@@ -1,2 +1,12 @@
 
 
+const side1 = document.querySelector('.side1');
+const side2 = document.querySelector('.side2');
+
+window.addEventListener('scroll', () => {
+    // While scrolling move left side of the picture to the left
+    side1.style.left = -window.scrollY + 'px';
+
+    // While scrolling move left side of the picture to the right
+    side2.style.left = window.scrollY + 'px';
+});
